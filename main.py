1df82435edbea5e42202f9d1cb65705984a53f37
Ayun Daywhea <32340123+citypop56709@users.noreply.github.com>
2023-01-07 22:52:30 +0300

    New methods to determine if a package is on truck 2 or not and a method to extract the time from the note. Time method still needs work to adjust to proper format.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from hash_table import HashTable
 from package import Package
+import pandas as pd
 
 
 def main():
     hash_table = HashTable()
-    hash_table.put("A", 10)
-    hash_table.put("B", 2)
-    print(hash_table)
-    print(hash_table.keys())
-    print(hash_table.values())
+    data = pd.read_excel(r"Documentation/WGUPS Package File.xlsx", skiprows=7)
+    for i in range(len(data)):
+        values = data.iloc[i]
+        new_package = Package(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7])
+
+
+
 
 
 if __name__ == '__main__':
--- package/Package.py
+++ package/Package.py
@@ -1,15 +1,45 @@
+import datetime
+from typing import Optional
+
+
 class Package:
-    def __init__(self):
-        self._id = None
-        self._address = None
-        self.city = None
-        self.state = None
-        self.zip = None
-        self.deadline = None
-        self.mass = None
-        self.truck = 0
+    def __init__(self, id:int, address:str, city:str, state:str, zip:str, deadline:object, mass:int, note:Optional[str]):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
+        self.mass = mass
         self.arrival = None
-    @property
-    def package_id(self):
-        print(f"The {self._id} was accessed.")
-        return (self._id)
+        self.note = note
+        self.truck2 = False
+
+    """    
+    Reads the memo and set's the truck2 flag to True if it requires Truck 2 for deliveries.
+    """
+    def is_truck2(self, note:str):
+        if "can only be on truck 2" in note.lower(): self.truck2 = True
+
+    """
+    Reads the memo and set's the arrival time from none to a special arrival time frame.
+    """
+    def find_arrival(self, note:str):
+        time = ''
+        new_string = ''.join([i for i in note.lower() if i.isalnum()])
+        for i in range(len(new_string)):
+            if new_string[i].isdigit():
+                time += new_string[i]
+            elif new_string[i] == ':':
+                time += new_string[i]
+            elif time and new_string[i:i+2] == 'am' or new_string[i:i+2] == 'pm':
+                time += new_string[i:i+2]
+            else:
+                pass
+        return time
+
+
+
+
+
+
